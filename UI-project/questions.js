7d6dff0f277741a484e8f92a1832ae506652cc6a
Sreenadh Venugopal <73292575+sri-nadh@users.noreply.github.com>
2022-11-27 21:20:16 +0530

    Delete UI-project directory

--- UI-project/questions.js
+++
@@ -1,7 +0,0 @@
-var questions = [
-  ["What is the name of the cosmic cube that Loki takes control of in The Avengers?", "the Tesseract", "the Magic Cube", "the Mjolnir", "all","1"],
-  [" How many Avengers are there in the Avengers movie?", " 4", " 10", "6", "100","3"],
-  [" What is the name of Tony Stark's company in the Iron Man trilogy?", "  Stark and Sons", "  Stark and Co", " Stark Industries", "Avg co","3"],
-  ["Who is Thor's main romantic interest in Thor?", "Jane Foster", "Jean Grey", "Mary Jane", "peta Jenson","1"],
-  ["What is the name of Rhodey's iron suit in Iron Man 3", "Death Proof", "  War Machine", " Killing Machine", "Jump Suit","1"],
-  ];
